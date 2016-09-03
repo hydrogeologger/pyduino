9ed05e2ebc7c74b5904ad2b7e133d26139ec80d8
chenming <chenming@debian>
2016-09-04 09:12:29 +1000

    put more details on the datareading function

--- python/read_arduino.py
+++ python/read_arduino.py
@@ -3,6 +3,18 @@ import serial
 import syslog
 import time
 # this script reads off from arduino on the go.
+# it is currently confirmed that arduino behaves similarly as all the scale sensors
+# 1. having internal buffers
+# 2. Being able to use simple serial interface to communicate with
+# currently it is decided to use the time in raspberry pi/PC to record the time that
+#    data collected by arduino for two reasons
+# 1. it is not known how much arduino time will be derivated over time.
+# 2. raspberry pi has internet connected and so has time weel syncronized. 
+# However, it is always important that the reading intervals in arduino has to be 
+#    the same as the current script reading interval. because the existing buffer in 
+#    arduino may cause the time deviation between the time arduino has been read and 
+#    the time raspberry has recorded the data
+
 
 
 port = '/dev/ttyUSB0'
