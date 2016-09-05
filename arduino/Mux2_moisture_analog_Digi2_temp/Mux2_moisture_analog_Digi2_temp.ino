1ddc1d30aa74639aac2242008e96faf785bd260c
chenming <chenming@debian>
2016-09-06 09:07:49 +1000

    functional codes

---
+++ arduino/Mux2_moisture_analog_Digi2_temp/Mux2_moisture_analog_Digi2_temp.ino
@@ -0,0 +1,185 @@
+/*This code combines: 
+ 1. mux schield 2 with 16 moisture sensor installed at port I/O 2,
+ 2. digital pin at port 2 with 10 temperature sensor (DS18B20) installed
+ Developed by Chenming TO160914
+ Cable wiring hints: 
+ 1. A 10k ohm pull down resistor ( brown black orange gold with yellow background)
+ needs to be installed between GND and I/O2 (i.e., A1)
+so that interferences between neighbouring ports are prevented. (once interference 
+occurrs, the downstream ports will have decreasing ghost values over the distance
+from the the port with sensor installed. although having a sensor installed downstream
+will prevent such )
+ 2. A 4.7k ohm (currenly red brown orange black green brown with light blue background )
+ pull up resistor needs to be installed between 5V and signal
+ 
+ */
+
+// -------------------- needed by mux schield -----------------------
+#include <MuxShield.h>
+//Initialize the Mux Shield
+MuxShield muxShield;
+
+char seperator=',';
+// -------------------- needed by digital sensor --------------------
+#include <OneWire.h>
+OneWire  ds(3);  // on digita pin 2 (a 4.7K resistor is necessary)
+
+
+
+
+void setup()
+{
+    // -------------------- needed by mux schield -----------------------
+     //open up the internal pull-up resistor
+     // it is confirmed that if using a 10k ohm +-5% resistor between 5+ and gnd, 
+     // the noise will be completely removed. This setup means to install a PULLDOWN
+     // Resistor between signal and ground TO160905.
+     // However: if both the resistor and the following code
+
+    //Set I/O 1, I/O 2, and I/O 3 as analog inputs
+    muxShield.setMode(1,ANALOG_IN);
+    muxShield.setMode(2,ANALOG_IN);
+    muxShield.setMode(3,ANALOG_IN);
+    Serial.begin(9600);
+    
+    // -------------------- needed by digital sensor-------------------
+}
+
+//Arrays to store analog values after recieving them
+//int IO1AnalogVals[16];
+int IO2AnalogVals[16];
+//int IO3AnalogVals[16];
+
+void loop()
+{
+  //digitalWrite(A1, LOW);
+  //digitalWrite(A0, LOW);
+  // Defining the waiting time between each readings;
+  int delay_mmsec=200;
+  int number_sensors=16;
+  for (int i=0; i<number_sensors; i++)
+  {
+    //Analog read on all 16 inputs on IO1, IO2, and IO3
+    //IO1AnalogVals[i] =0;
+    //IO1AnalogVals[i] = muxShield.analogReadMS(1,i);
+    IO2AnalogVals[i] = muxShield.analogReadMS(2,i);
+    //IO3AnalogVals[i] = muxShield.analogReadMS(3,i);
+    delay(delay_mmsec);
+  }
+  
+  //Print IO1 values for inspection
+  Serial.print("IO2 analog");
+  Serial.print(seperator);
+  for (int i=0; i<number_sensors; i++)
+  {
+    //Serial.print(IO1AnalogVals[i]);
+    Serial.print(IO2AnalogVals[i]);
+    Serial.print(seperator);
+  }
+  Serial.println();
+  delay(1000);
+
+
+// -------------------------------- required by onewire  -------------------
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+
+  if ( !ds.search(addr)) {
+    Serial.println("No more addresses.");
+    //Serial.println();
+    ds.reset_search();
+    delay(3000);
+    return;
+  }
+
+  Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return;
+  }
+  //Serial.println();
+
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      //Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      //Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      //Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      //Serial.println("Device is not a DS18x20 family device.");
+      return;
+  } 
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44);        // start conversion, use ds.write(0x44,1) with parasite power on at the end
+
+  delay(100);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+  //Serial.print("  Data = ");
+  //Serial.print(present, HEX);
+  //Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    //Serial.print(data[i], HEX);
+    //Serial.print(" ");
+  }
+  //Serial.print(" CRC=");
+  //Serial.print(OneWire::crc8(data, 8), HEX);
+  //Serial.println();
+
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  Serial.print(",  Temperature = ");
+  Serial.print(celsius);
+  Serial.print(" Celsius, ");
+  Serial.print(fahrenheit);
+  Serial.println(" Fahrenheit");
+  
+}
+ //
+//orange brown red black green brown with light blue background 312 Ohms 0.5% 100ppm//
+//brown green black red brown orange with light blue background 15k Ohms 1% 15 ppm//
+//need to check the resistance from volt meter//
+
--- python/read_arduino.py
+++ python/read_arduino.py
@@ -7,22 +7,71 @@ import time
 # 1. having internal buffers
 # 2. Being able to use simple serial interface to communicate with
 # currently it is decided to use the time in raspberry pi/PC to record the time that
-#    data collected by arduino for two reasons
+#    data collected by arduino for two reasons:
 # 1. it is not known how much arduino time will be derivated over time.
-# 2. raspberry pi has internet connected and so has time weel syncronized. 
+# 2. raspberry pi has internet connected and so has time well syncronized. 
+#
 # However, it is always important that the reading intervals in arduino has to be 
 #    the same as the current script reading interval. because the existing buffer in 
 #    arduino may cause the time deviation between the time arduino has been read and 
 #    the time raspberry has recorded the data
+# Also, user needs to close the monitoring interfce in arduino GUID, as each USB port
+#    can only be occupied by one process. 
 # It is found so far that arduino will store 500 undisplayed data, if these data has
 #    yet retrived, arduino will stop running as indicated by RX stopped flashing.
-#http://stackoverflow.com/questions/15184932/how-to-upload-source-code-to-arduino-from-bash
+# basic usage:
+#    moving the working directory to the folder where the python file exists:
+# In [1]: cd /home/chenming/pydrino/python
+#    execute the file
+# In [2]: execfile('read_arduino.py')
 
-
-
+### --------------------------input section ---------------------------
+# the port arduino has been connected to. in windows, it is usually 'COM4, COM5' where
+#   the number is subject to change. Just try 'devmgmt.msc' after pressing ctrl+r.
+# In linux it is usually /dev/ttyUSB
 port = '/dev/ttyUSB0'
-ard = serial.Serial(port,9600,timeout=5)
-msg = ard.readline()
-for i in range(1000): msg = ard.readline();print i,msg
 
-ard.close()
+# whether the result will be displayed on the screen
+screen_display=True
+
+# whether save the result as a file 
+save_to_file=True
+
+# the Filename of the csv file for storing file
+file_name= 'arduino_data.csv'
+
+# the time interval between each reading from arduino in seconds
+# be careful about the data collection interval in arduino, it is always good 
+# to make the sleep_time_seconds to be the same as the time interval in arduino
+# to minimize the data loss.
+# e.g., if arduino time interval is 1, python interval is 10, arduino will produce
+# more data than python can actually retrive, as a result, arduino may replace the 
+# existing data, cause the inconsistency between the data collection in arduino and 
+# data save in python
+# similar situation applies for commercial scales.
+sleep_time_seconds=1
+
+# number of readings, give a large value if you want to read the data in months
+#   but it should not be too big,
+no_reading=100000;
+
+# the delimiter between files, it is prefered to use ',' which is standard for csv file
+seperator=','
+### --------------------------- Processing data --------------------
+# open up the arduino port
+ard = serial.Serial(port,9600,timeout=1000)
+
+# the '0' at the end of the script helps save instantly.
+# http://stackoverflow.com/questions/18984092/python-2-7-wr
+if save_to_file: fid= open(file_name,'a',0)
+
+for i in xrange(no_reading): 
+    time_now=time.strftime("%d/%b/%Y %H:%M:%S")
+    msg = ard.readline()
+    if screen_display: print i,seperator,time_now,seperator,msg
+    if save_to_file: fid.write(time_now+seperator+msg)
+    time.sleep(sleep_time_seconds)
+
+fid.close()
+ser.close()
+
