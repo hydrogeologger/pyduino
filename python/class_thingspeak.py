2f88d84141254ced94585a6611e5c8bb3784f0cb
chenmingzhang <cmzhang1318@hotmail.com>
2016-12-08 23:23:24 +1000

    add class

---
+++ python/class_thingspeak.py
@@ -0,0 +1,21 @@
+import numpy as np
+import httplib, urllib
+
+def ts_upload(temp,key1):
+    while True:
+        fields={}
+        for i in np.arange(len(temp)):
+            fields['field'+str(i)]=temp[i]
+        fields['key']=key1
+        params = urllib.urlencode(fields)
+        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = httplib.HTTPConnection("api.thingspeak.com:80")
+        try:
+            conn.request("POST", "/update", params, headers)
+            response = conn.getresponse()
+            print response.status, response.reason
+            data = response.read()
+            conn.close()
+        except:
+            print "connection failed"
+        break
