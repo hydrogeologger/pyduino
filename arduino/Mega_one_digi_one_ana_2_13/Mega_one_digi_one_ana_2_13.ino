943defa015ba6fba66910d76a64bf934893cc24e
chenmingzhang <cmzhang1318@hotmail.com>
2016-12-19 04:17:06 +0000

    now analog working

--- arduino/Mega_one_digi_one_ana_2_13/Mega_one_digi_one_ana_2_13.ino
+++ arduino/Mega_one_digi_one_ana_2_13/Mega_one_digi_one_ana_2_13.ino
@@ -1,4 +1,10 @@
-AnalogVals1[12];
+
+// below is needed for analog sensor  
+
+static const uint8_t analog_pins[]  = {A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13};
+// the array below works for digital sensor arrays
+int digital_pins[] = {2, 3, 4, 5, 6, 7,8,9,10,11,12,13};
+float analogvals1[12];
 int i;
 // the delimiter between each reading. it is good to use ',' alwyas
 char seperator=',';
@@ -12,10 +18,10 @@ int dummy_readings=30;
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
-  serial.begin(9600);
+  Serial.begin(9600);
   // let all of the digital pins as output
   for (int i=0; i<number_sensors;i++){
-      pinmode(digital_pins[i],output);
+      pinMode(digital_pins[i],OUTPUT);
   }
 }
 
@@ -24,33 +30,33 @@ void loop() {
   // read the input on analog pin 0
   for (int i=0; i<number_sensors;i++){
     analogvals1[i]=0;
-    digitalwrite(digital_pins[i],high);
+    digitalWrite(digital_pins[i],HIGH);
     delay(1000);
 
     for (int j=0;j<dummy_readings;j++){
-      analogread(analog_pins[i]);
+      analogRead(analog_pins[i]);
       //delay(100);
     }
 
     for (int j=0;j<number_readings;j++){
-      analogvals1[i]+=analogread(analog_pins[i]);
+      analogvals1[i]+=analogRead(analog_pins[i]);
       delay(10);
     }
 
     analogvals1[i]=analogvals1[i]/number_readings;
-    digitalwrite(digital_pins[i],low);
+    digitalWrite(digital_pins[i],LOW);
  }
     
     for (int i=0; i<number_sensors;i++)
     {
     //serial.print(i);
     //serial.print(seperator);
-    serial.print(analogvals1[i]);
-    serial.print(seperator);
+    Serial.print(analogvals1[i]);
+    Serial.print(seperator);
 
     }
     
-    serial.println();
+    Serial.println();
     delay(5000);
     //delay_min(30);
     }
@@ -68,41 +74,7 @@ void delay_min(int min){
     }
   }
 }
-//    //digitalWrite(3,LOW);    
-//    //     
-//    i=1;
-//        Serial.print(i);
-//        Serial.print(seperator);
-//    digitalWrite(3,HIGH);
-//    delay(1000);
-//    //digitalWrite(5,HIGH);    
-//
-//
-//
-//
-//    for (int j=0;j<50;j++){
-//     analogRead(analog_pins[i]);
-//    delay(100);
-//    }
-//
-//
-//    
-//    for (int j=0;j<20;j++){
-//    AnalogVals1[i]=analogRead(analog_pins[i]);
-//    AnalogVals2[i]=analogRead(analog_pins[i]);
-//    Serial.print(AnalogVals2[i]);
-//    Serial.print(seperator);
-//    //Serial.println();
-//    delay(100);
-//    }
-//
-//
-//    
-//    Serial.print(i);
-//    Serial.println();
-//    digitalWrite(3,LOW);
-//    //digitalWrite(5,LOW);  
-//  //}
+
 
 /* I tested 
 16_10_14  one sensor,on port 1: water soaked value: 608
