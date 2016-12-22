16c7fc043616783ce62e4fd17e5a47876d81ab8b
chenmingzhang <cmzhang1318@hotmail.com>
2016-12-22 16:22:20 +1000

    now moisture for roof 1 is working

--- arduino/Mega_DS18x20_temp_sdi12_ana1_digi1_roof/Mega_DS18x20_temp_sdi12_ana1_digi1_roof.ino
+++ arduino/Mega_DS18x20_temp_sdi12_ana1_digi1_roof/Mega_DS18x20_temp_sdi12_ana1_digi1_roof.ino
@@ -69,7 +69,11 @@ void setup(void) {
 }
 
 void loop(void) {
+Serial.print("Soil");
+Serial.print(seperator);
 read_temp_sensors();
+ana_digi_loop();
+sdi12_loop();
 //delay_min(30);
 }
 
@@ -100,7 +104,7 @@ void ana_digi_loop() {
     {
     //serial.print(i);
     //serial.print(seperator);
-    Serial.print("M");
+    Serial.print("Mo");
     Serial.print(seperator);
     Serial.print(i);
     Serial.print(seperator);
@@ -152,112 +156,70 @@ void read_temp_sensors(void) {
   byte type_s;
   byte data[12];
   byte addr[8];
-  float celsius;//, //fahrenheit;
-  
-  if ( !ds.search(addr)) {
-    //Serial.println("No more addresses.");
-    //Serial.println();
-    Serial.print(delimiter);
-    ana_digi_loop();
-    sdi12_loop();
-    ds.reset_search();
-    //delay_min(30);
-    delay(100);
-    return;
-  }
+  float celsius;
 
-  /*
-  //Serial.print("ROM =");
-  for( i = 0; i < 8; i++) {
-    Serial.write(delimiter);
-    Serial.print(addr[i], HEX);
-    //Serial.print(addr[i].remove(5), HEX);
-  }
-  */
-  // here only the second addr is print as it is suffice to distinguish the address
-  Serial.write(delimiter);
-  Serial.write("T");
-  Serial.write(delimiter);
-  Serial.print(addr[1], HEX);
+  while ( ds.search(addr)) {
+      // here only the second addr is print as it is suffice to distinguish the address
+      Serial.write("Tp");
+      Serial.write(delimiter);
+      Serial.print(addr[1], HEX);
+        
+      if (OneWire::crc8(addr, 7) != addr[7]) {
+          Serial.println("CRC is not valid!");
+          return;
+      }
     
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return;
+      ds.reset();
+      ds.select(addr);
+      ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+      
+      delay(1000);     // maybe 750ms is enough, maybe not
+      // we might do a ds.depower() here, but the reset will take care of it.
+      
+      present = ds.reset();
+      ds.select(addr);    
+      ds.write(0xBE);         // Read Scratchpad
+    
+      //Serial.print("  Data = ");
+      //Serial.print(present, HEX);
+      //Serial.print(" ");
+      for ( i = 0; i < 9; i++) {           // we need 9 bytes
+        data[i] = ds.read();
+        //Serial.print(data[i], HEX);
+        //Serial.print(" ");
+      }
+      //Serial.print(" CRC=");
+      //Serial.print(OneWire::crc8(data, 8), HEX);
+      //Serial.println();
+    
+      // Convert the data to actual temperature
+      // because the result is a 16 bit signed integer, it should
+      // be stored to an "int16_t" type, which is always 16 bits
+      // even when compiled on a 32 bit processor.
+      int16_t raw = (data[1] << 8) | data[0];
+      if (type_s) {
+        raw = raw << 3; // 9 bit resolution default
+        if (data[7] == 0x10) {
+          // "count remain" gives full 12 bit resolution
+          raw = (raw & 0xFFF0) + 12 - data[6];
+        }
+      } else {
+        byte cfg = (data[4] & 0x60);
+        // at lower res, the low bits are undefined, so let's zero them
+        if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+        else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+        else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+        //// default is 12 bit resolution, 750 ms conversion time
+      }
+      celsius = (float)raw / 16.0;
+      Serial.print(delimiter);
+      Serial.print(celsius);
+      Serial.write(delimiter);
   }
-  //Serial.println();
- 
-  // the first ROM byte indicates which chip
-  /*
-  switch (addr[0]) {
-    case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20 family device.");
-      return;
-  } 
-  */
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
   
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-  
-  present = ds.reset();
-  ds.select(addr);    
-  ds.write(0xBE);         // Read Scratchpad
-
-  //Serial.print("  Data = ");
-  //Serial.print(present, HEX);
-  //Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = ds.read();
-    //Serial.print(data[i], HEX);
-    //Serial.print(" ");
-  }
-  //Serial.print(" CRC=");
-  //Serial.print(OneWire::crc8(data, 8), HEX);
-  //Serial.println();
-
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
-  }
-  celsius = (float)raw / 16.0;
-  //Serial.print(delimiter);
-  //Serial.print("  Temperature = ");
-  Serial.print(delimiter);
-  Serial.print(celsius);
-  //Serial.print(delimiter);
-  //Serial.print(" Celsius, ");
-  //Serial.print(delimiter);
-
-
+  ds.reset_search();
+  delay(100);
+  return;
 }
 
 void sdi12_loop(void){
@@ -420,7 +382,7 @@ char printInfo(char i){
     if(mySDI12.available()) mySDI12.read(); 
   }
 
-  Serial.write("ST");
+  Serial.write("SuTp");
   Serial.print(delimiter);
   while(mySDI12.available()){
     char c = mySDI12.read();
