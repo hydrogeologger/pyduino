3ae60f2ab60c963cc03b7a5506e71145bba9ed3e
chenmingzhang <cmzhang1318@hotmail.com>
2017-05-07 22:25:02 +1000

    add aster column

---
+++ arduino/column_aster/column_aster.ino
@@ -0,0 +1,90 @@
+static const uint8_t analog_pins[]  = {A0,A1,A2,A3,A4,A5,A6,A7,A8,A9};
+// the array below works for digital sensor arrays
+int digital_pins[] = {22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+float AnalogVals1[10];
+//float AnalogVals2[10];
+int i;
+// the delimiter between each reading. it is good to use ',' alwyas
+char seperator=',';
+//Arrays to store analog values after recieving them
+int number_sensors=10;
+// the setup routine runs once when you press reset:
+
+
+int number_readings=11;
+
+int dummy_readings=10;
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // let all of the pins as output
+
+ for (int i=0; i<number_sensors;i++){
+        pinMode(digital_pins[i],OUTPUT);
+    }
+}
+
+
+void loop(){
+    String content = "";
+    char character;
+    while(Serial.available()) {
+        character = Serial.read();
+        content.concat(character); 
+        delay (10); 
+    }
+    if (content != ""){
+        if (content == "All") { 
+            Serial.print("All");
+            Serial.print(delimiter);
+            read_moisture_loop();
+            Serial.println("AllDone");
+        }
+        else if (content == "SoilMoisture") {
+            Serial.print("SoilMoisture");
+            Serial.print(delimiter);
+            read_moisture_loop();
+            Serial.println("SoilMoistureDone");
+        }
+        else {
+          Serial.println(content);
+        } 
+    } //content != ""
+} //void loop
+
+
+
+
+// the loop routine runs over and over again forever:
+void read_moisture_loop() {
+  // read the input on analog pin 0
+  for (int i=0; i<number_sensors;i++){
+    AnalogVals1[i]=0.;
+    digitalWrite(digital_pins[i],HIGH);
+    delay(100);
+
+    for (int j=0;j<dummy_readings;j++){
+      analogRead(analog_pins[i]);
+      //delay(100);
+    }
+
+    for (int j=0;j<number_readings;j++){
+      AnalogVals1[i]+=analogRead(analog_pins[i]);
+      delay(10);
+    }
+
+    AnalogVals1[i]=AnalogVals1[i]/float(number_readings);
+    digitalWrite(digital_pins[i],HIGH);
+ }
+
+    for (int i=0; i<number_sensors;i++)
+    {
+    Serial.print(AnalogVals1[i]);
+    Serial.print(seperator);
+
+    }
+
+
+    }
+
+                                       
