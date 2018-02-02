961da916d6b5b86919203ed542ab0853b7465c5c
cmzhang1318 <cmzhang1318@hotmail.com>
2018-02-02 09:15:43 +0000

    add qal py

--- python/gpio_floodlight_camera.py
+++ python/gpio_floodlight_camera.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO            # import RPi.GPIO module
 from time import sleep             # lets us have a delay  
 import subprocess
 GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
-GPIO.setup(23, GPIO.OUT)           # set GPIO24 as an output   
-GPIO.output(23, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
+GPIO.setup(25, GPIO.OUT)           # set GPIO24 as an output   
+GPIO.output(25, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
 sleep(2)                 # wait half a second  
 subprocess.call("/home/pi/script/raspstill_snapshot_marandoo.sh", shell=True)
 sleep(2)                 # wait half a second  
