91a071d6bdcbfed0767e52168e2d271ef8ea7f5f
chenmingzhang <cmzhang1318@hotmail.com>
2018-10-23 14:24:37 +1000

    add phant weather

---
+++ particle/SparkFun_Photon_Weather_Phant/credentials.h
@@ -0,0 +1,9 @@
+// Replace with your actual SSID and password:
+
+//#define WIFI_SSID "Your SSID here"
+//#define WIFI_PASSWD "WLAN AP password here"
+#define SPARKFUN_SERVER_ADDR "144.6.225.24"
+#define SPARKFUN_SERVER_KEY_PUBLIC "L8ggD6e4K6TOdBX00bvOiYaNKrD"
+#define SPARKFUN_SERVER_KEY_PRIVATE "8Kbbnzw7Mzi3xw2NNeE3i4Ym23w"
+#define REPORT_INTERVAL_SECOND 600
+#define SPARKFUN_SERVER_PORT 8080
