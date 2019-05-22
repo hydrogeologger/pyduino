3c2ce8cdc7148cfa09f89b315ef443b8d3f23758
Jimmy <xuanth@live.com>
2019-05-22 16:20:42 +1000

    testing TB and IO

--- python/TB_io_test.py
+++ python/TB_io_test.py
@@ -1,26 +1,49 @@
 import paho.mqtt.client as mqtt
 import json
+import time
+import serial
 
-with open('/home/pi/pyduino/credential/humchamber.json') as f: credential = json.load(f) #,object_pairs_hook=collections.OrderedDict)
 
-while True:
-    try:
-        next_reading = time.time()
-        client = mqtt.Client()
-        client.username_pw_set(credential['access_token'])
-        client.connect(credential['thingsboard_host'], 1883, 60)
-        client.loop_start()
-        break
-    except Exception, e:
-        time.sleep(60)
-	break
 
-humchamber {'value1' : 1, 'value2' : 2}
-while True:
-    client.publish('v1/devices/me/telemetry', json.dumps(humchamber), 1)
-    sleep(1)
+port_sensor = '/dev/ttyS0' # port for serial connection
+ard=serial.Serial(port_sensor,timeout=60)
 
-client.loop_stop()
-client.disconnect()
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, rc, *extra_params):
+    print('Connected with result code ' + str(rc))
+    # Subscribing to receive RPC requests
+    client.subscribe('v1/devices/me/rpc/request/+')
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print('Topic: ' + msg.topic + '\nMessage: ' + str(msg.payload))
+    data = json.loads(msg.payload)
+    if data['method'] is 'setValue':
+        if data['params'] is True:
+            ard.write("power_switch,10,power_switch_status,0")
+            ard.flushInput()
+            ard.readline()
+        if data['params'] is False:
+            ard.write("power_switch,10,power_switch_status,255")
+            ard.flushInput()
+            ard.readline()
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.username_pw_set('TUdndmZQO0Ob3sTmM64y')
+client.connect('www.uqtailingsmonitoringengine.cloud.edu.au', 1883, 60)
+
+time.sleep(2)
+
+
+try:
+    client.loop_forever()
+except KeyboardInterrupt:
+    exit()
 
 
