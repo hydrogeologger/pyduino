96b9085ba48dc2953705bc9d6caa1cef653260d0
Levi <fulcan19@hotmail.com>
2021-02-03 10:25:52 +1000

    Updated utility/datalogger.v3_test.py
    + Added I2C Port Scanning
    + Added RPI Digital Pin testing as outputs
    * Modified definition name of rpi switch testing
    * Modified user input choice selection
    
    TODO: Comments

--- python/utility/datalogger.v3_test.py
+++ python/utility/datalogger.v3_test.py
@@ -14,7 +14,7 @@ import time
 # Introduce Compatibility for input() and raw_input() between python2 and python3
 PY3 = sys.version_info.major >= 3
 
-if sys.version_info.major == 3:
+if sys.version_info.major >= 3:
     pass
 elif sys.version_info.major == 2:
     try:
@@ -36,12 +36,14 @@ SERIAL_TIMEOUT_DEFAULT = 10 # Serial timeout in seconds
 RPI_RESET_PIN = 27  #GPIO/BCM pin number to reset arduino
 
 # RPI pins uses GPIO numbering (BCM)
-rpi_digital_pins = (2, 3, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21)
+rpi_digital_pins = (2, 17, 10, 11, 7, 13, 16, 21, 3, 18, 9, 8, 12, 19, 20) # Pins in physical board order
+# rpi_digital_pins = (2, 3, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21)
 rpi_switch_pins = (4, 5, 6, 22, 23, 24, 25, 26)
 arduino_swich_pins = (8, 9, 10, 11, 12, 13, 22, 23, 24, 25, 26, 27, 28,
         29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
         47, 48, 49)
-arduino_analog_pins = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+arduino_analog_pins = (0, 2, 4, 6, 8, 10, 12, 14, 1, 3, 5, 7, 9, 11, 13, 15) # Pins in physical order
+# arduino_analog_pins = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
 arduino_digital_only_pins = (5,)
 arduino_pwm_pins = (3, 4)
 arduino_sdi12_pins = (50, 51, 52, 53)
@@ -51,6 +53,7 @@ arduino_sdi12_pins = (50, 51, 52, 53)
 def toggle_debug(DEBUG):
     DEBUG = not DEBUG
     print("DEBUG Mode: " + str(DEBUG))
+    return DEBUG
 
 def is_rpi_digital_pin(element):
     element = int(element)
@@ -95,9 +98,9 @@ def perform_handshake():
     time.sleep(1)
     message_received = arduino_serial.readline()
     if (message_received == "abc\r\n"):
-        print("Hand Shake Success: received ABC response from Arduino")
+        print("Success Handshake: Received ABC response from Arduino")
     else:
-        print("Hand Shake Failed: No Response from Arduino")
+        print("Failed Handshake: No Response from Arduino")
 
 
 def reset_arduino(pin = RPI_RESET_PIN, sleep_duration = 5):
@@ -133,7 +136,7 @@ def onboard_dht22_test():
     #     print('humidity sensor reading failed')
 
 
-def rpi_digital_low_input_test():
+def rpi_digital_input_test():
     try:
         GPIO.setmode(GPIO.BCM)
         for pin in rpi_digital_pins:
@@ -150,9 +153,71 @@ def rpi_digital_low_input_test():
         GPIO.cleanup() #clean up rpi gpio setup when done
 
 
+def rpi_digital_output_test():
+    index = 0
+    GPIO.setmode(GPIO.BCM)
+
+    try:
+        user_timer = input("Duration of RPI IO state in seconds, default 5 seconds: ")
+        if (user_timer =='x'):
+            return
+        else:
+            user_timer = float(user_timer)
+    except ValueError:
+        #Defaults to 5 seconds if user input anything other than a number or x
+        user_timer = float(5)
+
+    try:
+        while index < len(rpi_digital_pins):
+            gpio_pin = rpi_digital_pins[index]
+
+            try:
+                user_pin = input("RPI IO pin, x escape, enter for #" + str(gpio_pin) + ": ")
+                if (user_pin == ""):
+                    pass
+                elif (is_escape(user_pin)):
+                    return
+                elif (is_rpi_digital_pin(user_pin)):
+                    gpio_pin = int(user_pin)
+                    index = rpi_digital_pins.index(int(user_pin))
+                else:
+                    continue
+
+                GPIO.setup(gpio_pin, GPIO.OUT)
+                GPIO.output(gpio_pin, GPIO.HIGH)
+
+                time.sleep(user_timer)
+
+                GPIO.output(gpio_pin, GPIO.LOW)
+                index = index + 1
+        
+            except ValueError:
+                # Prompt again if is incorrect power pin
+                continue
+
+    except Exception as error:
+        print("RPI digital GPIO Output test failed on pin " + str(gpio_pin))
+        print(type(error))
+        print(error)
+        
+    finally:
+        GPIO.cleanup()
+
+
+
 def rpi_switches_test():
     index = 0
     GPIO.setmode(GPIO.BCM)
+
+    try:
+        user_timer = input("Duration of switch state in seconds, default 5 seconds: ")
+        if (user_timer =='x'):
+            return
+        else:
+            user_timer = float(user_timer)
+    except ValueError:
+        #Defaults to 5 seconds if user input anything other than a number or x
+        user_timer = float(5)
 
     try:
         while index < len(rpi_switch_pins):
@@ -172,7 +237,9 @@ def rpi_switches_test():
 
                 GPIO.setup(power_pin, GPIO.OUT)
                 GPIO.output(power_pin, GPIO.HIGH)
-                time.sleep(2)
+
+                time.sleep(user_timer)
+
                 GPIO.output(power_pin, GPIO.LOW)
                 index = index + 1
         
@@ -181,7 +248,7 @@ def rpi_switches_test():
                 continue
 
     except Exception as error:
-        print("RPI digital GPIO input test failed on pin " + str(power_pin))
+        print("RPI Switch failed on Switch " + str(power_pin))
         print(type(error))
         print(error)
         
@@ -216,12 +283,17 @@ def sdi12_sensor_test():
             power_pin = input("Power Pin, 'x' to escape: ")
             if (is_escape(power_pin)):
                 return
-            elif (is_arduino_switch(power_pin) or power_pin == '-1'):
+            elif ((power_pin == "") or (power_pin == '-1')):
+                power_pin = -1
+                break
+            elif (power_pin.isalpha()):
+                continue
+            elif (is_arduino_switch(power_pin)):
                 break
                 
         except (ValueError):
-            # Set power pin to default value if no pin was given or incorrect pin
-            power_pin = -1
+            # Prompt for pin again if incorrect input
+            print("SDI12 Test, Power Pin ValueError")
             break
     
     try:
@@ -310,12 +382,17 @@ def arduino_analog_test():
             power_pin = input("Power Pin, 'x' to escape: ")
             if (is_escape(power_pin)):
                 return
-            elif ( is_arduino_switch(power_pin) or power_pin == '-1'):
+            elif ((power_pin == "") or (power_pin == '-1')):
+                power_pin = -1
+                break
+            elif (power_pin.isalpha()):
+                continue
+            elif (is_arduino_switch(power_pin)):
                 break
                 
         except (ValueError):
-            # Set power pin to default value if no pin was given or incorrect pin
-            power_pin = -1
+            # Prompt for pin again if incorrect input
+            print("Arduino Analog Test, Power Pin ValueError")
             break
 
     while index < len(arduino_analog_pins):
@@ -369,23 +446,69 @@ def check_arduino_runtime_since_last_comm():
     print(message_received.rstrip())
 
 
+def scan_i2c_multiplexer():
+    while True:
+        try:
+            power_pin = input("Power Pin to switch for duration of scan, 'x' to escape: ")
+            if (is_escape(power_pin)):
+                return
+            elif ((power_pin == "") or (power_pin == '-1')):
+                power_pin = -1
+                break
+            elif (power_pin.isalpha()):
+                continue
+            elif (is_arduino_switch(power_pin)):
+                break
+                
+        except (ValueError):
+            # Prompt for pin again if incorrect input
+            print("I2C Power Pin ValueError")
+            continue
+    
+    if is_arduino_switch(power_pin):
+        message_out = "power_switch,{0},power_switch_status,1".format(power_pin)
+        if DEBUG: print("DEBUG: " + message_out)
+        arduino_serial.write(message_out)
+        arduino_serial.flushInput()
+        time.sleep(1)
+
+    message_out = "9548_search"
+    if DEBUG: print("DEBUG: " + message_out)
+    arduino_serial.write(message_out)
+    arduino_serial.flushInput()
+    time.sleep(1)
+    message_received = ""
+    while message_received != "Done\r\n":
+        message_received = arduino_serial.readline()
+        print(message_received.rstrip())
+    
+    if is_arduino_switch(power_pin):
+        message_out = "power_switch,{0},power_switch_status,0".format(power_pin)
+        if DEBUG: print("DEBUG: " + message_out)
+        arduino_serial.write(message_out)
+        arduino_serial.flushInput()
+        time.sleep(1)
+
+
 
 def display_options_menu():
     print("Select the following options for [" + HARDWARE_NAME + "] testing:\n")
-    print("1:   Arduino Handshake Serial COM")
-    print("2:   Reset Arduino")
-    print("3:   Onboard DHT22 Humidity and Temperature Sensor")
-    print("4:   RPI Switches")
-    print("5:   RPI digital IO as INPUT")
-    print("6:   Arduino Switches")
-    print("7:   Arduino Analog IO as INPUT")
-    print("8:   SDI12 Sensor Ports")
-    print("9:   Get onboard DC voltage via VSENSE, requires vsense\n     and jumper (switch 9) set correctly")
-    print("10:  Check Arduino Runtime Since Last Communication")
-    print("m:   Display This Main Options Menu")
-    print("x:   Exit")
-    print("reset: Initiate RPI RESET request from Arduino")
-    print("debug: Debug Mode Toggle")
+    print("    1:   Arduino Handshake Serial COM")
+    print("    2:   Reset Arduino")
+    print("    3:   Onboard DHT22 Humidity and Temperature Sensor")
+    print("    4:   RPI Switches")
+    print("    5:   RPI digital IO as OUTPUT")
+    print("    6:   RPI digital IO as INPUT")
+    print("    7:   Arduino Switches")
+    print("    8:   Arduino Analog IO as INPUT")
+    print("    9:   SDI12 Sensor Ports")
+    print("   10:   Scan I2C Multiplexer")
+    print("   11:   Get onboard DC voltage via VSENSE, requires vsense\n         and jumper (switch 9) set correctly")
+    print("   12:   Check Arduino Runtime Since Last Communication")
+    print("    m:   Display This Main Options Menu")
+    print("    x:   Exit")
+    print("reset:   Initiate RPI RESET request from Arduino")
+    print("debug:   Debug Mode Toggle")
 
 
 #---------------------------- Main Loop ----------------------------------------
@@ -400,9 +523,6 @@ while True:
     try:
         print("")
         user_option = input("Option: ")
-        # except Exception as error:
-            # print(type(error))
-            # print(error)
 
         if (user_option == 'x'):
             break
@@ -415,28 +535,35 @@ while True:
         elif (user_option == '4'):
             rpi_switches_test()
         elif (user_option == '5'):
-            rpi_digital_low_input_test()
+            rpi_digital_output_test()
         elif (user_option == '6'):
-            arduino_switches_test()
+            rpi_digital_input_test()
         elif (user_option == '7'):
-            arduino_analog_test()
+            arduino_switches_test()
         elif (user_option == '8'):
-            sdi12_sensor_test()
+            arduino_analog_test()
         elif (user_option == '9'):
-            vsense_adc_check()
+            sdi12_sensor_test()
         elif (user_option == '10'):
+            scan_i2c_multiplexer()
+        elif (user_option == '11'):
+            vsense_adc_check()
+        elif (user_option == '12'):
             check_arduino_runtime_since_last_comm()
         elif (user_option == 'm'):
             display_options_menu()
         elif (user_option == 'reset'):
             reset_rpi_by_arduino()
         elif (user_option == 'debug'):
-            toggle_debug(DEBUG)
+            DEBUG = toggle_debug(DEBUG)
 
     except KeyboardInterrupt:
         arduino_serial.close()
         print("")
         exit()
+    # except Exception as error:
+            # print(type(error))
+            # print(error)
 
 #---------------------------- Cleaning Up --------------------------------------
 arduino_serial.close()
