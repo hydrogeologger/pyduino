0968530e794c588da6b1399cc3fc70bc1a50b13f
xuanth <xuanth@live.com>
2021-03-13 22:36:39 +1000

    with yuanchi to make a template to send photo to cloud

--- bash/camera_photo.sh
+++ bash/camera_photo.sh
@@ -1,9 +1,10 @@
 echo "wait for 120 second"
-sleep 240
+#sleep 240
 #need enough time for pi to reboot and load modules required by camera, and connect to wifi if this script is run by crontab on reboot, this sleep is importtant because crontab start to count time before wifi is ready, if sleep time is too short, user may not even able to see the username command prompt, particularly when the script is end up with shutdown.
 
-cd /home/pi/photo_weather_station/
-DATE=$(date +"%Y-%m-%d_%H%M")_field_camera.jpg
+source $pyduino/bash/pyduino_variables.sh
+cd $local_photo_address
+DATE=$(date +"%Y-%m-%d_%H%M")_sa3.jpg
 
 
 echo "taking photos"
@@ -11,4 +12,4 @@ raspistill -vf  -o $DATE  2>> camera_error
 echo "sleep for 5 sec"
 sleep 5
 
-scp /home/pi/photo_weather_station/$DATE username@servername:/storage/www/xxdu.au/username/folder/  2>> camera_error
+scp $local_photo_address/$DATE $ssh_scp_photo_address  2>> camera_error
