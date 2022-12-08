f3602b06fcd43d4246948987781165c3acc726c7
Levi <fulcan19@hotmail.com>
2022-12-08 14:39:16 +1000

    Added bluez NoInputNoOutput pairing bug workaround

--- install/bluetooth_pan/install.sh
+++ install/bluetooth_pan/install.sh
@@ -2,6 +2,7 @@
 
 # Script for initial setup/configuration of either RaspberryPI or BananaPI system
 # https://raspberrypi.stackexchange.com/a/71587
+# try bluez-5.50-1.2~deb10u3
 
 # Check if script is running as root
 if [ "$EUID" -ne 0 ]
@@ -14,6 +15,45 @@ BT_CONF_DNSMASQ="0"       # do not install dnsmasq (supported values: 0|1)
 BT_CONF_HIDDEN="0"        # system is permanently discoverable/pairable
                           # supported values: 0|1
 
+function bluez_noinputnooutput_bug_displayonly_globalpin_workaround() {
+    # Bug workaround from https://github.com/RPi-Distro/repo/issues/291
+    local CONF_FILE="/home/${SUDO_USER:-$USER}/bt-agent-pins.conf"
+    readonly CONF_FILE
+
+    echo "Implementing bluez NoInputNoOutput bug workaround using DisplayOnly Capability with global pin..."
+
+    # Create pin file to allow all passcode for all device mac
+    echo "* *" > "$CONF_FILE"
+
+    # Change bt-agent launch parameter
+    # sed -i -e "s/^[^#]\(.*\)\(NoInputNoOutput\)/# \1\2/" \
+    #         -e "s/^\#\s*\(.*\)\(DisplayOnly\)\(.*\)/\1\2\3/" \
+    #         /etc/systemd/system/bt-agent.service
+    sed -i -e "s/NoInputNoOutput/DisplayOnly -p ${CONF_FILE//\//\\/}/" \
+            /etc/systemd/system/bt-agent.service
+}
+
+function bluez_noinputnooutput_bug_justworkrepairing_setting_workaround() {
+    # Bug workaround from https://github.com/bluez/bluez/issues/93
+    # https://stackoverflow.com/questions/75362887/linuxbluezbluetooth-failed-to-re-connect-after-forgetting-while-using-noin
+    echo "Implementing bluez NoInputNoOutput bug workaround using JustWorksRepairing bluetooth conf setting..."
+
+    sed -i -E "s/^#?\s*JustWorksRepairing\s*=.*$/JustWorksRepairing = always/" \
+            /etc/bluetooth/main.conf;
+}
+
+function revert_bluez_noinputnooutput_bug_workaround() {
+    local CONF_FILE="/home/${SUDO_USER:-$USER}/bt-agent-pins.conf"
+    readonly CONF_FILE
+
+    if [ -f "$CONF_FILE" ]; then
+        echo "File '$CONF_FILE' exists. Removing..."
+        rm "$CONF_FILE"
+    fi
+
+    sed -i -e "/JustWorksRepairing/s/^.*$/#JustWorksRepairing = never/" \
+            /etc/bluetooth/main.conf
+}
 
 # shellcheck source=SCRIPTDIR/../common.sh
 source "$(dirname "${BASH_SOURCE[0]}")/../common.sh"
@@ -38,6 +78,25 @@ chmod 644 /etc/systemd/system/bt-agent.service
 chmod 644 /etc/systemd/system/bt-network.service
 chown root:root /etc/systemd/system/bt-agent.service
 chown root:root /etc/systemd/system/bt-network.service
+
+echo "" # Empty to print newline
+echo "Would you like you implement bluez NoInputNoOutput pairing workaround?"
+select option in "DisplayOnly-GlobalPin" "JustWorksRepairing" "None/Revert"; do
+    case $option in
+        "DisplayOnly-GlobalPin")
+            revert_bluez_noinputnooutput_bug_workaround;
+            bluez_noinputnooutput_bug_displayonly_globalpin_workaround;
+            break;;
+        "JustWorksRepairing")
+            revert_bluez_noinputnooutput_bug_workaround;
+            bluez_noinputnooutput_bug_justworkrepairing_setting_workaround;
+            break;;
+        * )
+            revert_bluez_noinputnooutput_bug_workaround;
+            true; break;;
+    esac
+done
+
 
 # Unblock bluetooth
 rfkill unblock bluetooth
@@ -70,4 +129,3 @@ fi
 # Non fullproof workaround, disable the service
 systemctl stop systemd-networkd-wait-online.service
 systemctl disable systemd-networkd-wait-online.service
-
