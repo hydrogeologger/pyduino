17bebf87cbc3deeadc1b4fb626fc7b752db6fb6b
Levi <fulcan19@hotmail.com>
2023-11-08 16:14:53 +1000

    pylib-postprocess: Interpolation module initial commit
    Added interpolation class methods and made variables more descriptive
    
    Use internal reference for interpolated date time output
    
    Stopped using numpy.timedelta, converted to using timedelta.total_seconds()
    
    interpolate_smooth takes dataframe or series
    
    Allow renaming of interpolated column heading
    
    Added ability to initialize Interpolation object with data.
    Added Interpolated.__str__ magic method
    
    Import matplotlib at start of script
    
    Added explicit call of pandas.plotting.register_matplotlib_converters() for pandas version 0.24.x
    
    Add docstring and formatting fixes
    
    Data type checks will raise TypeError

--- python/lib/postprocess/__init__.py
+++ python/lib/postprocess/__init__.py
@@ -1,7 +1,12 @@
 """This package contains modules and functions to assist with post processing.
 
 Dependencies:
+- matplotlib
+- numpy
+- pandas
 """
 
 # Module Info
 __version__ = "0.1.0"
+
+from .interpolation import *
