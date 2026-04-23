14718fb9dc571feec2f230fb6f6f4e31080848e7
Levi <fulcan19@hotmail.com>
2026-04-23 14:59:17 +1000

    pylib-postprocess: Add open-meteo api wrapper to sources subpackage
    Add sources subpackage and sources.utils module.
    Add `requests` to list of project dependencies

---
+++ python/lib/postprocess/src/postprocess/sources/__init__.py
@@ -0,0 +1,10 @@
+"""Interfaces for retrieving and handling data from external sources.
+
+The modules in this package provide integrations with external APIs and
+encapsulate the logic required to request, retrieve, and handle source-specific
+data structures.
+"""
+
+__all__ = [
+    "openmeteo"
+]
